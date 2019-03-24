abf6ad24b256661c5369a4eaf8aa3405ad3f4789
Mark Baggett <mbagget1@utk.edu>
2019-03-23 22:56:40 -0400

    Add Rhodes Farnsworth test.

--- tests/validate_scenarios.sh
+++ tests/validate_scenarios.sh
@@ -18,6 +18,12 @@ testValidityOfSternberg() {
     assertEquals "${STERNBERG}" "${TESTFILE} validates"
 }
 
+testValidityofFarnsworth() {
+    ${SAXON} ${SAMPLEDATA}/Rhodes/Farnsworth.xml ${STYLESHEETS}/rhodes_dspace_xoai_to_mods_farnsworth.xsl 2>&1 2>/dev/null 1>${TESTFILE}
+    STERNBERG=$(xmllint --noout --schema ${DLTNMODS} ${TESTFILE} 2>&1 1>/dev/null | cat)
+    assertEquals "${STERNBERG}" "${TESTFILE} validates"
+}
+
 # This unit test expects all records in Sample_Data/Crossroads to use rhodes_dspace_xoai_to_mods.xsl
 testValidityOfCrossroadsXOAItoMODS() {
     for filename in ${SAMPLEDATA}/Crossroads_xoai_default/*.xml; do
