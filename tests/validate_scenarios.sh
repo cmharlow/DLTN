5b760dac87ece814b338bcd790781c80eb8f18da
Mark Baggett <mbagget1@utk.edu>
2019-03-24 22:59:41 -0400

    Add test for country qdc.

--- tests/validate_scenarios.sh
+++ tests/validate_scenarios.sh
@@ -12,6 +12,7 @@ testJavaInstalled() {
     assertNotNull $(which java)
 }
 
+# Rhodes and Crossroads to Freedom
 testValidityOfSternberg() {
     ${SAXON} ${SAMPLEDATA}/Rhodes/sternberg.xml ${STYLESHEETS}/rhodes_sternberg_xoai_to_mods.xsl 2>&1 2>/dev/null 1>${TESTFILE}
     STERNBERG=$(xmllint --noout --schema ${DLTNMODS} ${TESTFILE} 2>&1 1>/dev/null | cat)
@@ -20,11 +21,10 @@ testValidityOfSternberg() {
 
 testValidityofFarnsworth() {
     ${SAXON} ${SAMPLEDATA}/Rhodes/Farnsworth.xml ${STYLESHEETS}/rhodes_dspace_xoai_to_mods_farnsworth.xsl 2>&1 2>/dev/null 1>${TESTFILE}
-    STERNBERG=$(xmllint --noout --schema ${DLTNMODS} ${TESTFILE} 2>&1 1>/dev/null | cat)
-    assertEquals "${STERNBERG}" "${TESTFILE} validates"
+    RESPONSE=$(xmllint --noout --schema ${DLTNMODS} ${TESTFILE} 2>&1 1>/dev/null | cat)
+    assertEquals "${RESPONSE}" "${TESTFILE} validates"
 }
 
-# This unit test expects all records in Sample_Data/Crossroads to use rhodes_dspace_xoai_to_mods.xsl
 testValidityOfCrossroadsXOAItoMODS() {
     for filename in ${SAMPLEDATA}/Crossroads_xoai_default/*.xml; do
         ${SAXON} ${filename} ${STYLESHEETS}/rhodes_dspace_xoai_to_mods.xsl 2>&1 2>/dev/null 1>${TESTFILE}
@@ -32,5 +32,13 @@ testValidityOfCrossroadsXOAItoMODS() {
         assertEquals "${RESPONSE}" "${TESTFILE} validates"
     done
 }
+
+# Country Music Hall of Fame Tests
+testValidityOfCountryMusicHallofFame() {
+    ${SAXON} ${SAMPLEDATA}/Country/countryqdc.xml ${STYLESHEETS}/countryqdctomods.xsl 2>&1 2>/dev/null 1>${TESTFILE}
+    RESPONSE=$(xmllint --noout --schema ${DLTNMODS} ${TESTFILE} 2>&1 1>/dev/null | cat)
+    assertEquals "${RESPONSE}" "${TESTFILE} validates"
+}
+
 
 . shunit2
